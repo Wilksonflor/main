708c29c4b98655a2b9a9bb69be8a085c56e6d09d
Wilkson Flor Soares <90102289+Wilksonflor@users.noreply.github.com>
2022-08-30 14:04:37 -0300

    Delete Estudos - HTML5, CSS3 e JavaScript/DESAFIOS - CODELANDIA/Desafio 27 directory

--- Estudos - HTML5, CSS3 e JavaScript/DESAFIOS - CODELANDIA/Desafio 27/Assets/js/main.js
+++
@@ -1,6 +0,0 @@
-const btnMobile = document.getElementById('btn-mobile');
-function toggleMenu(){
-  const btnMobile = document.getElementById('nav');
-  nav.classList.toggle('active')//Classlist vai ativar a classe de ativo quando clicar e remover ao clicar.
-}
-btnMobile.addEventListener('click', toggleMenu);
