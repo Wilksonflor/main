dad348794c01287d10895d4cc399864857c89b4c
Wilkson Flor Soares <90102289+Wilksonflor@users.noreply.github.com>
2022-09-14 22:10:14 -0300

    Add files via upload

--- Estudos - HTML5, CSS3 e JavaScript/DESAFIOS - CODELANDIA/DESAFIOS - CODELANDIA/DESAFIO 04 - TELA DE LOGIN/script.js
+++ Estudos - HTML5, CSS3 e JavaScript/DESAFIOS - CODELANDIA/DESAFIOS - CODELANDIA/DESAFIO 04 - TELA DE LOGIN/script.js
@@ -1,31 +1,13 @@
-const form = document.getElementById('form')
-const email = document.getElementById('email')
-const senha = document.getElementById('senha')
-const entrar = document.getElementById('entrar')
-const entrarGoogle = document.getElementById('loginGoogle')
-
+let email = document.getElementById('email')
+let senha = document.getElementById('senha')
+let form = document.querySelector('form')
+let textForm = document.getElementById('textForm')
 form.addEventListener('submit', e => {
-  //o (e) tira o comportamento padrão do browser
-  e.preventDefault() //vai cancelar o comportamento padrão ou seja, quando clicar em enviar ele irá atualizar
-  checkInputs()
-})
-
-function checkInputs() {
-  //validações dos campos
-
-  const emailValue = email.value.trim() //criei a variavel e irá pegar o valor(value)
-  const senhaValue = senha.value.trim() //trim irá remover os espaçamentos
-  if (emailValue === '') {
-    //mostrar o error
-    //adicionar a classe de error
-
-    errorValidation(email, 'Preencha esse campo')
+  if (email.value == '' && senha.value == '') {
+    textForm.textContent = 'Você precisa digitar esse campo'
   } else {
-    //caso tenha digitado, mostrará a classe suces
+    console.log(email.value)
+    console.log(senha.value)
   }
-}
-
-function errorValidation(input, message) {
-  const form = input.parentElement //parent retona a referencia do pai
-  inputsLogin.className = ''
-}
+  e.preventDefault()
+})
